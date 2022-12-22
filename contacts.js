dd2b4999a07888751422393d53fd7f070cb791ef
vitaliy-kro <krochakv6@gmail.com>
2022-12-22 22:31:08 +0200

    add __dirname in path

--- contacts.js
+++ contacts.js
@@ -1,7 +1,7 @@
 const fs = require("fs").promises;
 const path = require("path");
 
-const contactsPath = path.resolve("./db/contacts.json");
+const contactsPath = path.resolve(__dirname, "db/contacts.json");
 
 async function getContacts() {
   const read = await fs.readFile(contactsPath);
